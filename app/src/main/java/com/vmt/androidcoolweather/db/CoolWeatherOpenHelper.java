136ddd89fd978656ba5563f251c051978862de7f
SilverBullet5563 <2441928193@qq.com>
2016-08-28 13:52:52 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/vmt/androidcoolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/vmt/androidcoolweather/db/CoolWeatherOpenHelper.java
@@ -9,29 +9,29 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
-        /*
+        /**
         * Province表建表语句
         */
-    public static final String CREAT_PROVINCE = "create table Province("
+    public static final String CREATE_PROVINCE = "create table Province("
             + "id integer primary key autoincrement,"
             + "province_name text,"
             + "province_code text)";
-        /*
+        /**
         *   city表
         */
-    public static final String CREAT_CITY = "create table City("
+    public static final String CREATE_CITY = "create table City("
             + "id integer primary key autoincrement, "
-            + "city_name text, "
-            + "city_code text, "
+            + "city_name text,"
+            + "city_code text,"
             + "province_id integer)";
 
-        /*
+        /**
         *   country表
         */
-    public static final String CREATE_COUNTRY = "create table Country"
+    public static final String CREATE_COUNTRY = "create table Country("
                 + "id integer primary key autoincrement, "
-                + "country_name text, "
-                + "country_code text, "
+                + "country_name text,"
+                + "country_code text,"
                 + "city_id integer)";
 
     public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -40,9 +40,9 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREAT_CITY);
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
         db.execSQL(CREATE_COUNTRY);
-        db.execSQL(CREAT_PROVINCE);
     }
 
     @Override
